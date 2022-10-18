285f1f9433c5a22170322e36623a1eac4585e9f3
Sergey Petrinich <petrinichshadow@gmail.com>
2022-10-18 21:09:55 +0300

    firebase deploy

---
+++ README.md
@@ -0,0 +1,4 @@
+## Links
+
+<a href="https://pet-cappadochia.web.app">Home Page [1 link]</a> \
+<a href="https://pet-cappadochia.firebaseapp.com">Home Page [2 link]</a>
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,39 +1,10 @@
-/*
-Документация по работе в шаблоне: 
-Документация слайдера: https://swiperjs.com/
-Сниппет(HTML): swiper
-*/
-
-// Подключаем слайдер Swiper из node_modules
-// При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
-// Пример: { Navigation, Autoplay }
 import Swiper, { Navigation, Keyboard } from "swiper";
-/*
-Основниые модули слайдера:
-Navigation, Pagination, Autoplay, 
-EffectFade, Lazy, Manipulation
-Подробнее смотри https://swiperjs.com/
-*/
 
-// Стили Swiper
-// Базовые стили
 import "../../scss/base/swiper.scss";
-// Полный набор стилей из scss/libs/swiper.scss
-// import "../../scss/libs/swiper.scss";
-// Полный набор стилей из node_modules
-// import 'swiper/css';
 
-// Инициализация слайдеров
 function initSliders() {
-    // Перечень слайдеров
-    // Проверяем, есть ли слайдер на стронице
     if (document.querySelector(".tail__swiper")) {
-        // Указываем скласс нужного слайдера
-        // Создаем слайдер
         new Swiper(".tail__swiper", {
-            // Указываем скласс нужного слайдера
-            // Подключаем модули слайдера
-            // для конкретного случая
             modules: [Navigation, Keyboard],
             observer: true,
             observeParents: true,
@@ -44,33 +15,19 @@ function initSliders() {
             loop: true,
 
             keyboard: {
-                // Включить\выключить
                 enabled: true,
-                // Включить\выключить
-                // только когда слайдер
-                // в пределах вьюпорта
                 onlyInViewport: true,
-                // Включить\выключить
-                // управление клавишами
-                // pageUp, pageDown
                 pageUpDown: true,
             },
             navigation: {
                 nextEl: ".tail__arrow_next",
                 prevEl: ".tail__arrow_prev",
             },
-
-            on: {},
         });
     }
 
     if (document.querySelector(".slider-trip__body")) {
-        // Указываем скласс нужного слайдера
-        // Создаем слайдер
         new Swiper(".slider-trip__body", {
-            // Указываем скласс нужного слайдера
-            // Подключаем модули слайдера
-            // для конкретного случая
             modules: [Navigation, Keyboard],
             observer: true,
             observeParents: true,
@@ -80,30 +37,18 @@ function initSliders() {
             speed: 800,
             loop: true,
             keyboard: {
-                // Включить\выключить
                 enabled: true,
-                // Включить\выключить
-                // только когда слайдер
-                // в пределах вьюпорта
                 onlyInViewport: true,
-                // Включить\выключить
-                // управление клавишами
-                // pageUp, pageDown
                 pageUpDown: true,
             },
             navigation: {
                 nextEl: ".trip__arrow_next",
                 prevEl: ".trip__arrow_prev",
             },
-
-            on: {},
         });
     }
 }
 
 window.addEventListener("load", function (e) {
-    // Запуск инициализации слайдеров
     initSliders();
-    // Запуск инициализации скролла на базе слайдера (по классу swiper_scroll)
-    //initSlidersScroll();
 });
